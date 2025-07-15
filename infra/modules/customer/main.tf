739dbdb15d81bc94c58042188e77ae1a8537d13b
limalucas99 <lucaslim.lux@gmail.com>
2025-07-15 19:14:07 -0300

    feat: create rds and net configurations for customer service

---
+++ infra/modules/customer/main.tf
@@ -0,0 +1,53 @@
+data "aws_vpc" "default" { default = true }
+
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "customer-db-subnet-group"
+  subnet_ids = data.aws_subnets.subnets.ids
+}
+
+resource "aws_security_group" "customer-db-sg" {
+  name   = "customer-db-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    description      = "Customer DB Remote Access"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]   
+    ipv6_cidr_blocks = ["::/0"]       
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_db_instance" "customer_service_db" {
+  identifier              = "customer-service-db"
+  db_name                 = "customer_service_db"
+  engine                  = "postgres"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 10
+  username                = "lucas"
+  password                = "12345678"  
+  publicly_accessible     = true
+
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.customer-db-sg.id]
+
+  skip_final_snapshot     = true
+  storage_encrypted       = true
+}
+
