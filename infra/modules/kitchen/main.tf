3f6e6ecef5c9f7f396b62a0a7c6d78ef1fe22da2
limalucas99 <lucaslim.lux@gmail.com>
2025-07-18 11:24:38 -0300

    feat: change kitchen module to use variables

--- infra/modules/kitchen/main.tf
+++ infra/modules/kitchen/main.tf
@@ -40,8 +40,8 @@ resource "aws_db_instance" "kitchen_service_db" {
   engine                  = "postgres"
   instance_class          = "db.t3.micro"
   allocated_storage       = 10
-  username                = "lucas"
-  password                = "12345678"  
+  username                = var.kitchen_db_username
+  password                = var.kitchen_db_password
   publicly_accessible     = true
 
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
---
+++ infra/modules/kitchen/variables.tf
@@ -0,0 +1,9 @@
+variable "kitchen_db_username" {
+  description = "Username for the kitchen database"
+  type        = string
+}
+
+variable "kitchen_db_password" {
+  description = "Password for the kitchen database"
+  type        = string
+}
