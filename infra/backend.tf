27c7b3c77ceef78f16febf29aebeacc800c87a33
limalucas99 <lucaslim.lux@gmail.com>
2025-07-22 19:08:58 -0300

    refactor: update s3 bucket used for back-end

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tc4-microservices-k8s-bucket"
+    bucket       = "tc4-microservices-k8s"
     key          = "microservices-k8s/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
