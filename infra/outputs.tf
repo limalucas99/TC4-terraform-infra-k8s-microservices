8259a0512b0c5ef2c9ef645b4e809e93532a403f
limalucas99 <lucaslim.lux@gmail.com>
2025-07-22 19:10:23 -0300

    refactor: comment unnecessary outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,11 @@
-output "customer_db_endpoint" {
-  value = module.customer.db_endpoint
-}
+# output "customer_db_endpoint" {
+#   value = module.customer.db_endpoint
+# }
 
-output "kitchen_db_endpoint" {
-  value = module.kitchen.db_endpoint
-}
+# output "kitchen_db_endpoint" {
+#   value = module.kitchen.db_endpoint
+# }
 
-output "payment_db_endpoint" {
-  value = module.payment.db_endpoint
-}
+# output "payment_db_endpoint" {
+#   value = module.payment.db_endpoint
+# }
